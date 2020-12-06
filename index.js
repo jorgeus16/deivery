7aae57932b3c97bdb648065f176e4c0c7960c27a
jorgeus16 <jorgechucena@gmailcom>
2020-12-06 22:26:42 +0000

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var DataStore = require('nedb');
 
 var port = 3000;
 var BASE_API_PATH = "/api/v1";
-
-var orders = [
-{"date" : "02/05/2020", "state" : "delivered"},
-{"date" : "25/09/2020", "state" : "delivered"}
-];
+var DB_FILE_NAME = __dirname + "/orders.json";
 
 console.log("Starting API server...");
 
 var app = express();
+app.use(bodyParser.json());
+
+var db = new DataStore({
+    filename: DB_FILE_NAME,
+    autoload: true
+});
 
 app.get("/", (req, res) =>{
     res.send("<html><body><h1>My Server</h1></body></html>");
 });
 
-app.get(BASE_API_PATH + "/orders", (req,res) => {
+app.get(BASE_API_PATH + "/orders", (req, res) => {
     console.log(Date() + " - GET /orders");
-    res.send(orders)
+    res.send([]);
+});
+
+app.post(BASE_API_PATH + "/orders", (req, res) => {
+    console.log(Date() + " - POST /orders");
+    var order = req.body;
+    db.insert(order, (err) => {
+        if(err) {
+            console.log(Date() + " - " + err);
+        }else{
+            res.sendStatus(201);
+        }
+    });
 });
 
 app.listen(port);
